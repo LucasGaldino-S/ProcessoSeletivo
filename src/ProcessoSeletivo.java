b2313bba489ac8fd9d559ff6eebfe6634c5638f6
LucasGaldino-S <galdinosaraiva@gmail.com>
2024-08-09 13:29:51 -0300

    lista de candidatos

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -2,7 +2,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-       selecaoCandidatos();
+       imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for (int indice = 0; indice < candidatos.length;indice++){
+            System.out.println("o candidato de numero " + (indice+1) + " e " + candidatos[indice]);
+
+
+            System.out.println("forma abreviada for each");
+            for (String candidato : candidatos){
+                System.out.println("o candidato selecionado foi " + candidato);
+            }
+        }
     }
 
     static void selecaoCandidatos(){
